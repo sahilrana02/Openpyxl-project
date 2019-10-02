e3dba0da5cd6d6057d2066325b709daa73d9017d
SAHIL RANA <sahilrana02@gmail.com>
2019-10-02 14:24:15 +0530

    final after modifications

--- FILE1.py
+++ FILE1.py
@@ -4,6 +4,7 @@ import re
 wb= op.load_workbook(r'C:\Users\RANA\Desktop\PFA\Quantum-II-MIM-IA_Final.xlsx')
 Outbound=wb.get_sheet_by_name('Outbound')
 Index=wb.get_sheet_by_name('Index')
+Vendor_sub=[]
 
 def rep2ndrow(r,c,job):
     with open(r'C:\Users\RANA\Desktop\PFA\3 New\3 New\{}\MIM\SQL\insert-JOB-DEV-SYS-INT-PRD.sql'.format(job),'r') as obj:
@@ -16,14 +17,20 @@ def rep2ndrow(r,c,job):
     Outbound.cell(row=r,column=c+1).value=file
 
 def PC_FILEID(job):
+    Fid=''
+    Vendor=['DMT','INTERAC','TSYS','BMO','TELUS']
     with open(r'C:\Users\RANA\Desktop\PFA\3 New\3 New\{}\MIM\SQL\insert-JOB-DEV-SYS-INT-PRD.sql'.format(job),'r') as obj:
         for i in obj:
             if re.search(",\d\d\d\d\);",i):
-                break
-    id=i.strip()
-    id=id.replace(',','F'+id[4])
-    id=id.replace('0);','')
-    return id
+                Fid=i.strip()
+            for v in Vendor:
+                if v in i :
+                    global Vendor_sub
+                    Vendor_sub=i.strip().replace(',\'','').replace('\'','').split(' ')
+                    break
+    Fid=Fid.replace(',','F'+Fid[4])
+    Fid=Fid.replace('0);','')
+    return Fid
 
 def edit_Outbound(j):
     mcol=Outbound.max_column
@@ -36,22 +43,25 @@ def edit_Outbound(j):
             Outbound.cell(row=i,column=mcol+1).value=j
 
 def edit_index(j):
+    global Vendor_sub
     lst=[]
     for r in range(1,Index.max_row+1):
             if Index.cell(row=r,column=1).value == j:
                 lst.append(r)#this will give row names for multiple entries
-    if len(lst)>1:#incase there are more than one rows considering atleast one row fulfills the criteria
-        for x in lst:
-            if Index.cell(row=x,column=3).value==PC_FILEID(j.rstrip()):
-                lst.remove(x)#eliminate correct row from consideration
-        for w in lst[::-1]:
-            Index.delete_rows(w,1) #delete all other rows from last
-    elif len(lst)==1:           #incase only one entry found
+
+    if lst:
         Index.cell(row=lst[0],column=3).value=PC_FILEID(j.rstrip())
+        Index.cell(row=lst[0],column=4).value=Vendor_sub[0]
+        Index.cell(row=lst[0],column=2).value=Vendor_sub[1]+"BOUND"
+        lst.pop(0)#eliminate 1st occurance from consideration
+        for d in lst[::-1]:  #delete all other rows
+            Index.delete_rows(d,1)
+
+    else:print("No entry found for: "+j)
 
 with open(r"C:\Users\RANA\Desktop\PFA\R1 Jobs1.txt","r") as obj:
     for j in obj:
-        edit_Outbound(j)
-        edit_index(j)
+        edit_Outbound(j.rstrip())
+        edit_index(j.rstrip())
 
 wb.save(r'C:\Users\RANA\Desktop\PFA\Quantum-II-MIM-IA_Final.xlsx')
