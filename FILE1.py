7f6aa25ebcb9adba1193955079cb6f7f9a3eefec
SAHIL RANA <sahilrana02@gmail.com>
2019-10-03 00:09:52 +0530

    Hyperlink functionality added and user can now make selection for inboud and outbound

--- FILE1.py
+++ FILE1.py
@@ -1,8 +1,9 @@
 import openpyxl as op
 from copy import copy
 import re
+Hyper_list=[]
 wb= op.load_workbook(r'C:\Users\RANA\Desktop\PFA\Quantum-II-MIM-IA_Final.xlsx')
-Inbound=wb.get_sheet_by_name('Outbound')
+Inbound=wb.get_sheet_by_name('Inbound')
 Outbound=wb.get_sheet_by_name('Outbound')
 Index=wb.get_sheet_by_name('Index')
 Vendor_sub=[]
@@ -42,6 +43,8 @@ def edit_Inbound(j):
             rep2ndrow(i,mcol,j.rstrip())
         elif i==4:
             Inbound.cell(row=i,column=mcol+1).value=j
+            global Hyper_list
+            Hyper_list=['Inbound',i,mcol+1]
 
 def edit_Outbound(j):
     mcol=Outbound.max_column
@@ -52,18 +55,26 @@ def edit_Outbound(j):
             rep2ndrow(i,mcol,j.rstrip())
         elif i==4:
             Outbound.cell(row=i,column=mcol+1).value=j
+            global Hyper_list
+            Hyper_list=['Outbound',i,mcol+1]
 
-def edit_index(j):
+def Hyperlink(r):
+    global Hyper_list
+    Index.cell(row=r, column=1).hyperlink= ("Quantum-II-MIM-IA_Final.xlsx#{}!{}".format(Hyper_list[0],str(Index.cell(row=Hyper_list[1], column=Hyper_list[2])).replace('>','').split('.')[1]))
+    Hyper_list=[] #Resetting the hyperlist
+
+def edit_Index(j):
     global Vendor_sub
+    Vendor_sub=[] #Resetting the vendor list
     lst=[]
     for r in range(1,Index.max_row+1):
             if Index.cell(row=r,column=1).value == j:
                 lst.append(r)#this will give row names for multiple entries
-
     if lst:
         Index.cell(row=lst[0],column=3).value=PC_FILEID(j.rstrip())
         Index.cell(row=lst[0],column=4).value=Vendor_sub[0]
         Index.cell(row=lst[0],column=2).value=Vendor_sub[1]+"BOUND"
+        Hyperlink(lst[0])
         lst.pop(0)#eliminate 1st occurance from consideration
         for d in lst[::-1]:  #delete all other rows
             Index.delete_rows(d,1)
@@ -71,9 +82,15 @@ def edit_index(j):
     else:print("No entry found for: "+j)
 
 with open(r"C:\Users\RANA\Desktop\PFA\R1 Jobs1.txt","r") as obj:
+
+    print("1:Inbound")
+    print("2:Outbound")
+    User_Input=int(input("Make selection:"))
     for j in obj:
-        edit_Inbound(j.rstrip())
-        edit_Outbound(j.rstrip())
-        edit_index(j.rstrip())
+        if User_Input==1:
+            edit_Inbound(j.rstrip())
+        elif User_Input==2:
+            edit_Outbound(j.rstrip())
+        edit_Index(j.rstrip())
 
 wb.save(r'C:\Users\RANA\Desktop\PFA\Quantum-II-MIM-IA_Final.xlsx')
--- hyperlink section.py
+++
@@ -1,8 +0,0 @@
-import openpyxl
-import re
-wb=openpyxl.load_workbook(r'C:\Users\RANA\Desktop\Book1.xlsx')
-index=wb.get_sheet_by_name('Index')
-
-index.cell(row=3, column=1).hyperlink = ("Book1.xlsx#sheet1!{}".format(str(index.cell(row=4, column=3)).replace('>','').split('.')[1]))
-
-wb.save(r'C:\Users\RANA\Desktop\Book1.xlsx')
